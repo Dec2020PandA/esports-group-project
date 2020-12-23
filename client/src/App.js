8c5eb31e1b7ea69e2148320c960827ae8a746f67
tntttom <tntt_tom@me.com>
2020-12-23 00:50:24 -0600

    initial setup

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+
+function App() {
+  const [socket] = useState(() => io(":8000"));
+
+  useEffect(() => {
+    console.log("Is this running?");
+    socket.on("Welcome", (data) => console.log(data));
+
+    // note that we're returning a callback function
+    // this ensures that the underlying socket will be closed if App is unmounted
+    // this would be more critical if we were creating the socket in a subcomponent
+    return () => socket.disconnect(true);
+  }, []);
+
+  const sendGame = (e) => {
+    e.preventDefault();
+    socket.emit("new_game", { gameName: "Valorant" });
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <button onClick={sendGame}>Click me to Emit</button>
+    </div>
+  );
+}
+
+export default App;
