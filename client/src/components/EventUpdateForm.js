c9afabfb092621af89f394bfea84d55f78856fc9
tnguyen28 <tntt_tyler@me.com>
2021-01-04 14:08:19 -0600

    change handling/state

--- client/src/components/EventUpdateForm.js
+++ client/src/components/EventUpdateForm.js
@@ -2,6 +2,30 @@ import React from "react";
 import "../App.css";
 
 class EventUpdateForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      t1Name: "",
+      t2Name: "",
+      t1MapScore: 0,
+      t2MapScore: 0,
+      t1RKScore: 0,
+      t2RKScore: 0,
+      eventName: "",
+      bestOf: "",
+      timeStart: "",
+    };
+  }
+
+  handleChange = (event) => {
+    let name = event.target.name;
+    let value = event.target.value;
+    this.setState({ [name]: value });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert("submitted update form");
+  };
   render() {
     return (
       <div>
@@ -10,34 +34,62 @@ class EventUpdateForm extends React.Component {
             <div>
               <div>
                 <label>Team 1</label>
-                <input type="text" name="teamOne" />
+                <input type="text" name="t1Name" onChange={this.handleChange} />
 
                 <label>Map Score</label>
-                <input type="number" name="mapScore" className="numberInput" />
+                <input
+                  type="number"
+                  name="t1MapScore"
+                  className="numberInput"
+                  onChange={this.handleChange}
+                />
 
                 <label>Round/Kill Score</label>
-                <input type="number" name="rkScore" className="numberInput" />
+                <input
+                  type="number"
+                  name="t1RKScore"
+                  className="numberInput"
+                  onChange={this.handleChange}
+                />
               </div>
               <div>
                 <label>Team 2</label>
                 <input type="text" name="teamTwo" />
                 <label>Map Score</label>
-                <input type="number" name="mapScore" className="numberInput" />
+                <input
+                  type="number"
+                  name="t2MapScore"
+                  className="numberInput"
+                  onChange={this.handleChange}
+                />
 
                 <label>Round/Kill Score</label>
-                <input type="number" name="rkScore" className="numberInput" />
+                <input
+                  type="number"
+                  name="t2RKScore"
+                  className="numberInput"
+                  onChange={this.handleChange}
+                />
               </div>
               <div>
                 <label>Event Name</label>
-                <input type="text" name="eventName" />
+                <input
+                  type="text"
+                  name="eventName"
+                  onChange={this.handleChange}
+                />
               </div>
               <div>
                 <label>Best Of</label>
-                <input type="text" name="bestOf" />
+                <input type="text" name="bestOf" onChange={this.handleChange} />
               </div>
               <div>
                 <label>Time Start</label>
-                <input type="text" name="timeStart" />
+                <input
+                  type="text"
+                  name="timeStart"
+                  onChange={this.handleChange}
+                />
               </div>
 
               <input type="submit" value="Update" />
