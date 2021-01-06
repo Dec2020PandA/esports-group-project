eb3d5183dcc8839a4e539fe46d7c8d8fa4084035
tntttom <tntt_tom@me.com>
2021-01-06 12:11:17 -0600

    Top Bar placeholder + styling

--- client/src/views/dashboard.js
+++ client/src/views/dashboard.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 
+import TopBar from "../components/TopBar";
 import Sidebar from "../components/Sidebar";
 
 export default () => {
   return (
     <div>
+      <TopBar />
       <Sidebar />
     </div>
   );
