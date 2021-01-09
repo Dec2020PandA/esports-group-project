1e4ad1cc9ae12e9df425f991007413bc76703092
tntttom <tntt_tom@me.com>
2021-01-09 13:45:42 -0600

    Minor styling changes for container

--- client/src/views/dashboard.js
+++ client/src/views/dashboard.js
@@ -3,15 +3,21 @@ import React, { useState, useEffect } from "react";
 import TopBar from "../components/TopBar";
 import Sidebar from "../components/Sidebar";
 
+import styles from "../css/Dashboard.module.css";
+
 export default (props) => {
   console.log(props.games);
   return (
     <div>
       <TopBar />
-      {props.games.map((game, index) => (
-        <h1 key={index}>{game.gameType}</h1>
-      ))}
-      <Sidebar />
+      <div className={styles.mainContent}>
+        <Sidebar />
+        {props.games.map((game, index) => (
+          <h1 className={styles.game} key={index}>
+            {game.gameType}
+          </h1>
+        ))}
+      </div>
     </div>
   );
 };
