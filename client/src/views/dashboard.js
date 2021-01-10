1aad8a7154a647a734ec23208b9a830b3c13ee48
tntttom <tntt_tom@me.com>
2021-01-09 18:27:43 -0600

    Dashboard layout set

--- client/src/views/dashboard.js
+++ client/src/views/dashboard.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import TopBar from "../components/TopBar";
 import Sidebar from "../components/Sidebar";
 
+import EventScoreCard from "../components/EventScoreCard";
+
 import styles from "../css/Dashboard.module.css";
 
 export default (props) => {
@@ -12,11 +14,11 @@ export default (props) => {
       <TopBar />
       <div className={styles.mainContent}>
         <Sidebar />
-        {props.games.map((game, index) => (
-          <h1 className={styles.game} key={index}>
-            {game.gameType}
-          </h1>
-        ))}
+        <div className={styles.featured}>
+          {props.games.map((game, index) => (
+            <EventScoreCard />
+          ))}
+        </div>
       </div>
     </div>
   );
